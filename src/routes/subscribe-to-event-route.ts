2c6db68288d655e8111bcdd4387e646e55eeb2d6
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-04-20 10:14:46 -0300

    add zod with fastify and documentation with swagger

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
+    app.post(
+        "/subscriptions",
+        {
+            schema: {
+                summary: "Subscribes someone to the event",
+                tags: ["subscription"],
+                description: "More details about this route",
+                body: z.object({
+                    name: z.string(),
+                    email: z.string().email(),
+                }),
+                response: {
+                    201: z.object({
+                        name: z.string(),
+                        email: z.string(),
+                    }),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { name, email } = request.body;
+
+            return reply.status(201).send({
+                name,
+                email,
+            });
+        }
+    );
+};
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,42 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import {
+    validatorCompiler,
+    serializerCompiler,
+    ZodTypeProvider,
+    jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+// Responsável por fazer a serialização, transforma os dados antes de enviar para o frontend
+app.setSerializerCompiler(serializerCompiler);
+
+// Responsável por fazer a validação utilizando o zod como plugin para fastify
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "NLW Connect",
+            version: "0.0.1",
+        },
+    },
+    transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+    routePrefix: "/docs",
+});
+
+app.register(subscribeToEventRoute);
 
 app.register(fastifyCors, {
     // Somente as URLs desse frontend poderá acessar este backend em especifico
-    origin: "http://localhost:3000",
-});
-
-app.get("/hello", () => {
-    return "hello world";
+    origin: "http://localhost:3333",
 });
 
 app.listen({ port: 3333 }).then(() => {
