daf237a9067d4b366ef23f0f5b600c367e092afe
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-04-21 13:32:00 -0300

    create a subscriber

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { subscribeToEvent } from "../functions/subscribe-to-event";
+import { subscribe } from "diagnostics_channel";
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
     app.post(
@@ -15,8 +17,7 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
                 }),
                 response: {
                     201: z.object({
-                        name: z.string(),
-                        email: z.string(),
+                        subscriberId: z.string(),
                     }),
                 },
             },
@@ -24,9 +25,10 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
         async (request, reply) => {
             const { name, email } = request.body;
 
+            const { subscriberId } = await subscribeToEvent({ name, email });
+
             return reply.status(201).send({
-                name,
-                email,
+                subscriberId,
             });
         }
     );
